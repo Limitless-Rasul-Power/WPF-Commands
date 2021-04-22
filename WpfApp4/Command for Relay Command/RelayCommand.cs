956485cc0a9f35ae63876c28174ca6d936df052f
Osmanli Resul Ferhad <Osma_th97@itstep.edu.az>
2021-04-22 17:42:11 +0400

    First Commit

---
+++ WpfApp4/Command for Relay Command/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp4.Command_for_Relay_Command
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add    { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute ?? throw new NullReferenceException();
+            _canExecute = canExecute;
+
+        }
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke(parameter);
+        }
+    }
+}
---
+++ WpfApp4/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp4.Command_for_Relay_Command;
+using WpfApp4.Commands;
+
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region RelayCommand
+
+        public RelayCommand MessageCommand { get; set; }
+        public RelayCommand SendCommand { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+            MessageCommand = new RelayCommand(Display);
+            SendCommand = new RelayCommand(Send);
+        }
+
+
+        public void Display(object parameter = null)
+        {
+            MessageBox.Show("Hello World.");
+        }
+
+        public void Send(object parameter = null)
+        {
+            MessageBox.Show("Send Successfully.");
+        }
+
+        #endregion
+
+        #region DelegateCommand
+
+
+        //public MessageCommand MessageCommand { get; set; }
+        //public SendCommand SendCommand { get; set; }
+        //public MainWindow()
+        //{
+        //    InitializeComponent();
+
+        //    this.DataContext = this;
+        //    MessageCommand = new MessageCommand(Display);
+        //    SendCommand = new SendCommand(Send);
+
+        //}
+        //public string Text { get; set; }
+
+
+        //public void Display()
+        //{
+        //    MessageBox.Show("Hakuna Matata.");
+        //}
+
+        //public void Send()
+        //{
+        //    MessageBox.Show("Message was sent successfully.");
+        //}
+
+        #endregion
+
+        #region SingleUseCommand
+
+        //public MainWindow()
+        //{
+        //    InitializeComponent();
+        //    this.DataContext = this;
+        //}
+
+        //public string Text { get; set; }
+        //private void Help_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        //{
+        //    e.CanExecute = Text?.Length > 10 ? true : false;
+        //}
+
+        //private void Help_Executed(object sender, ExecutedRoutedEventArgs e)
+        //{            
+        //    Process.Start("Notepad.exe");
+        //}
+
+        #endregion
+
+    }
+}
